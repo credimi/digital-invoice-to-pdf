b5159207395735b19883d3db92c47733570d1e2b
Davide Di Pumpo <davide.dipumpo@credimi.com>
2019-03-20 15:03:17 +0100

    start point

--- build/sum.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.sum = function(a, b) {
-  return a + b
-}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,2 @@
-import dotenv from 'dotenv'
-import express from 'express'
-dotenv.config()
-
-// Create a new express application instance
-const app: express.Application = express()
-
-app.get('/', (req, res) => {
-  res.send(`Hello ${process.env.APP_ID}!`)
-})
-
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
-})
+const Hello: string = 'Hello World'
+console.log(Hello)

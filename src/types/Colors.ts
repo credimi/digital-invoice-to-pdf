0f291c92c0b9e1468d9acf3e92cde57a9c89b9be
Davide Di Pumpo <davide.dipumpo@credimi.com>
2019-03-20 18:13:18 +0100

    adds options type

---
+++ src/types/Colors.ts
@@ -0,0 +1,7 @@
+export interface Colors {
+  primary?: string
+  text?: string
+  lighterText?: string
+  footerText?: string
+  tableHeader?: string
+}
---
+++ src/types/Options.ts
@@ -0,0 +1,7 @@
+import { Colors } from './Colors'
+
+export interface Options {
+  colors: Colors
+  footer: boolean
+  locale?: string
+}
